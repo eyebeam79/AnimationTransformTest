0c5f16c23ccf104b966138b6f0e0e517585ce157
Jinho Son <Tobi@Jinho-Son-ui-MacBook-Pro.local>
2014-01-07 12:55:31 +0900

    Initial Commit

---
+++ AnimationTransformTestTests/AnimationTransformTestTests.m
@@ -0,0 +1,34 @@
+//
+//  AnimationTransformTestTests.m
+//  AnimationTransformTestTests
+//
+//  Created by Jinho Son on 2014. 1. 7..
+//  Copyright (c) 2014년 STD1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AnimationTransformTestTests : XCTestCase
+
+@end
+
+@implementation AnimationTransformTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
